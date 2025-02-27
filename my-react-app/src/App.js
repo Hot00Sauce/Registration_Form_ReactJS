02f02d6e8f0625ee945b7d34790fcfa3282a48ca
Hot00Sauce <johnrollycedillo15@gmail.com>
2025-02-27 12:34:47 +0800

    just added props method practice

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,6 @@
 import Card from './Card';
 import Button from './Button';
+import Student from './Student';
 
 function App() {
   return (
@@ -9,6 +10,11 @@ function App() {
     <Card/>
     <Card/>
     <Button/>
+    <Student name="John Rolly Cedillo" age="hello" isStudent={true}/>
+    <Student name="John Doe" age={30} isStudent={false}/>
+    <Student name="Cynthia Aburame" age={80} isStudent={false}/>
+    <Student name="Angelica Dogde" age={20} isStudent={true}/>
+    <Student name="Larry" age={17} isStudent={true}/>
     </>
   );
 }
---
+++ my-react-app/src/Student.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+
+function Student(props) {
+    return(
+        <>
+        <div className="student">
+            <p>Name: {props.name}</p>
+            <p>age: {props.age}</p>
+            <p>student: {props.isStudent ? "Accept" : "Reject"}</p>
+        </div>
+        </>
+    );
+}
+
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isstudent: PropTypes.bool,
+}
+
+Student.defaultTypes = {
+    name: "Guest",
+    age: 0,
+    isStudent: false,
+}
+
+export default Student
